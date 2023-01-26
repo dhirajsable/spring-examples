82ac01313912221966fedbae8cd87c0010358cbf
Dhiraj Sable <sable.dhiraj@gmail.com>
2023-01-25 23:09:37 -0800

    Project Skeleton for Spring boot service .

---
+++ entity-service/src/main/java/com/example/entityservice/EntityServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.entityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityServiceApplication.class, args);
+    }
+}
---
+++ entity-service/src/test/java/com/example/entityservice/EntityServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.entityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
