3378198a04ff75f9474fa3c0b18126272b202215
Dhiraj Sable <sable.dhiraj@gmail.com>
2023-01-26 22:01:46 -0800

    Added classes

---
+++ entity-models/src/main/java/com/spring/example/requests/BookData.java
@@ -0,0 +1,22 @@
+package com.spring.example.requests;
+
+public class BookData {
+    private String bookName;
+    private String bookDescription;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookDescription() {
+        return bookDescription;
+    }
+
+    public void setBookDescription(String bookDescription) {
+        this.bookDescription = bookDescription;
+    }
+}
